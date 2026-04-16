428643c37cda482cd09cbd3eba8119730f4b010a
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-04-15 23:47:54 -0300

    feat(post): implement PublishedPost struct with methods and tests
    Add the PublishedPost struct to manage published posts, including methods for
    creating a new post, retrieving content, and retracting to a DraftPost.
    Unit tests are included to ensure functionality.

---
+++ src/post/published.rs
@@ -0,0 +1,52 @@
+use super::DraftPost;
+
+pub(crate) struct PublishedPost {
+    content: Box<str>,
+}
+
+impl PublishedPost {
+    pub(super) fn new(content: &str) -> Self {
+        Self {
+            content: content.into(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+
+    fn retract(self) -> DraftPost {
+        DraftPost::new(&self.content)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PublishedPost;
+
+    #[test]
+    fn new_keeps_content_unchanged() {
+        let post = PublishedPost::new("untouched content");
+        assert_eq!("untouched content", post.content());
+    }
+
+    #[test]
+    fn content_returns_stored_content() {
+        let post = PublishedPost::new("stored content");
+        assert_eq!("stored content", post.content());
+    }
+
+    #[test]
+    fn retract_keeps_content_unchanged() {
+        let post = PublishedPost::new("untouched content");
+        let post = post.retract();
+        assert_eq!("untouched content", post.content());
+    }
+
+    #[test]
+    fn retract_consumes_self() {
+        let takes_ownership: fn(PublishedPost) -> _ = PublishedPost::retract;
+        let post = PublishedPost::new("");
+        takes_ownership(post);
+    }
+}
